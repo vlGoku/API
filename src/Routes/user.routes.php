fefae6270c66baaa5d0fa261ec4646a88371ba46
vlGoku <markolucic21@outlook.de>
2024-06-24 10:55:15 +0200

    user.routes updated

--- src/Routes/user.routes.php
+++ src/Routes/user.routes.php
@@ -5,3 +5,11 @@ namespace Ml\Routes;
 echo 'Hello from user.routes';
 
 $action = $_REQUEST['action'] ?? null;
+
+enum UserAction {
+    case CREATE = 'create';
+    case GET = 'get';
+    case REMOVE = 'remove';
+    case UPDATE = 'update';
+    case GET_ALL = 'get_all';
+}
